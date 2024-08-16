1908bf05aaa45060113725e5f7a53efbc2d4d5e4
Pablo <parusov.pashka@ya.ru>
2024-08-16 17:52:41 +0300

    create repo's for flat and house

---
+++ internal/repositories/flat/flat_repo.go
@@ -0,0 +1,65 @@
+package flat
+
+import (
+	"apartment_search_service/internal/models"
+	openapi "apartment_search_service/internal/openapi/gen"
+	"apartment_search_service/internal/repositories"
+	"database/sql"
+	"fmt"
+)
+
+type flatRepository struct {
+	db *sql.DB
+}
+
+func NewFlatRepository(db *sql.DB) repositories.FlatRepository {
+	return &flatRepository{db: db}
+}
+
+func (f *flatRepository) Insert(flat *models.Flat) error {
+	query := `INSERT INTO flats (house_id, price, rooms, status) VALUES ($1, $2, $3, $4) RETURNING id`
+	err := f.db.QueryRow(query, flat.HouseId, flat.Price, flat.Rooms, flat.Status).
+		Scan(&flat.Id)
+	return err
+}
+
+func (f *flatRepository) UpdateStatus(flatId int, moderId, status string) (*models.Flat, error) {
+	flat := &models.Flat{}
+	query := `
+		UPDATE flats SET status = $1, moderator_id = $2
+		WHERE id = $3 
+		AND ((status = $4 AND moderator_id = $2) -- end moderation for current moderator
+		OR (status = $5 OR status != $4))        -- start new moderation
+		RETURNING id, house_id, price, rooms, status, moderator_id`
+	err := f.db.QueryRow(query, status, moderId, flatId, openapi.ON_MODERATION, openapi.CREATED).
+		Scan(&flat.Id, &flat.HouseId, &flat.Price, &flat.Rooms, &flat.Status, &flat.ModeratorId)
+	if err != nil {
+		return nil, err
+	}
+	return flat, nil
+}
+
+func (f *flatRepository) GetFlatsByHouseId(houseId int, isModer bool) ([]*models.Flat, error) {
+	query := `SELECT id, house_id, price, rooms, status, moderator_id FROM flats WHERE house_id=$1`
+	if !isModer {
+		query = fmt.Sprintf(`%s AND status = '%s'`, query, openapi.APPROVED)
+	}
+
+	rows, err := f.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	flats := make([]*models.Flat, 0, 32)
+	for rows.Next() {
+		flat := &models.Flat{}
+		err := rows.Scan(&flat.Id, &flat.HouseId, &flat.Price, &flat.Rooms, &flat.Status, &flat.ModeratorId)
+		if err != nil {
+			return nil, err
+		}
+		flats = append(flats, flat)
+	}
+
+	return flats, nil
+}
---
+++ internal/repositories/house/house_repo.go
@@ -0,0 +1,33 @@
+package house
+
+import (
+	"apartment_search_service/internal/models"
+	"apartment_search_service/internal/repositories"
+	"database/sql"
+)
+
+type houseRepository struct {
+	db *sql.DB
+}
+
+func NewHouseRepository(db *sql.DB) repositories.HouseRepository {
+	return &houseRepository{db: db}
+}
+
+func (hr *houseRepository) Insert(house *models.House) error {
+	query := `INSERT INTO houses (address, year, developer, created_at) VALUES ($1, $2, $3, $4) RETURNING id`
+	err := hr.db.QueryRow(query, house.Address, house.Year, house.Developer, house.CreatedAt).
+		Scan(&house.Id)
+	return err
+
+}
+
+func (hr *houseRepository) GetById(id int) (*models.House, error) {
+	house := &models.House{}
+	query := `SELECT id, address, year, developer, created_at, updated_at FROM houses WHERE id = $1`
+	if err := hr.db.QueryRow(query, id).
+		Scan(&house.Id, &house.Address, &house.Year, &house.Developer, &house.CreatedAt, &house.UpdatedAt); err != nil {
+		return nil, err
+	}
+	return house, nil
+}
