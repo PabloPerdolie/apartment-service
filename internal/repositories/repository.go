c39aaac620e195de947eea30738455a1407a0eee
Pablo <parusov.pashka@ya.ru>
2024-08-16 16:15:27 +0300

    interfaces for flat and house

--- internal/repositories/repository.go
+++ internal/repositories/repository.go
@@ -7,3 +7,14 @@ type UserRepository interface {
 	GetById(id string) (*models.User, error)
 	Get(email, password string) (*models.User, error)
 }
+
+type HouseRepository interface {
+	Insert(house *models.House) error
+	GetById(id int) (*models.House, error)
+}
+
+type FlatRepository interface {
+	Insert(flat *models.Flat) error
+	UpdateStatus(flatId int, moderId, status string) (*models.Flat, error)
+	GetFlatsByHouseId(houseId int, isModer bool) ([]*models.Flat, error)
+}
