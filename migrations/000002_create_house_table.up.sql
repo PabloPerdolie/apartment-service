f2058140492e390744623de515b9c688bb09bd4a
Pablo <parusov.pashka@ya.ru>
2024-08-16 16:09:41 +0300

    migrations for flat and house

---
+++ migrations/000002_create_house_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS houses (
+    id SERIAL PRIMARY KEY,
+    address VARCHAR(255) NOT NULL UNIQUE,
+    year INT NOT NULL,
+    developer VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
---
+++ migrations/000003_create_flat_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS flats (
+    id SERIAL PRIMARY KEY,
+    house_id INT NOT NULL REFERENCES houses(id),
+    price INT NOT NULL,
+    rooms INT NOT NULL,
+    status VARCHAR(13),
+    moderator_id VARCHAR(36) REFERENCES users(id)
+);
